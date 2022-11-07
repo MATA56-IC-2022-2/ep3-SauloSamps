284dc78e63afe4f1f98333a23c575d0f56e08937
Saulo Sampaio <saulo.samb2@gmail.com>
2022-11-07 08:52:29 -0300

    Update myappend.el

--- EP3/myappend.el
+++ EP3/myappend.el
@@ -1,5 +1,11 @@
-(defun myappend (list-1 list-2)
-	(cons list-1 list-2)
+(defun myappend (lst tail)
+    (if (null lst)
+      tail
+        (cons (first lst)
+            (myappend (rest lst) tail)
+		)
+	)
 )
-
-(write(myappend '(1 2 3) '(4 5 6)))
+					
+					
+(write(myappend '(1 2 3) '(4 5 6))) ;should return (1 2 3 4 5 6)
